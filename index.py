bd6f5b6146339dca87678826f5103558ae98a7e9
Coose.wang <wangqizhi1987@gmail.com>
2014-09-28 08:38:08 +0100

    add index

---
+++ index.py
@@ -0,0 +1,16 @@
+#/usr/bin/python
+#encoding = utf-8
+
+import web
+
+urls = (
+    '/', 'index'
+)
+
+class index:
+    def GET(self):
+        return "Hello, world!"
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
