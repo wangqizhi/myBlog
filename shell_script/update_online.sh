fbc38793f3c5d1e7f01aa20d26bdd16029e20894
coose.wang <wangizhi1987@gmail.com>
2014-09-28 16:07:39 +0800

    add README

--- shell_script/update_online.sh
+++ shell_script/update_online.sh
@@ -1,2 +1,3 @@
 #/bin/bash
 /usr/bin/rsync --progress -av --exclude="blog_env.conf" /root/src/git_hub/pyweber/* /usr/local/src/myblog/online
+/usr/bin/python /usr/local/src/myblog/online/index.py >/dev/null 2>&1  &
