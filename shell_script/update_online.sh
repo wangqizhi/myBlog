86ba2d5d647022a7cbaf53b29c8794ff1281954b
coose.wang <wangizhi1987@gmail.com>
2014-11-25 16:35:00 +0800

    modify start shell port

--- shell_script/update_online.sh
+++ shell_script/update_online.sh
@@ -2,4 +2,4 @@
 /usr/bin/rsync --progress -av --exclude="blog_env.conf" /root/src/git_hub/pyweber/* /usr/local/src/myblog/online
 PID=`/bin/ps -ef |grep [p]ython |awk -F " " '{printf $2}'`
 /bin/kill $PID
-/usr/bin/python /usr/local/src/myblog/online/index.py >/dev/null 2>&1  &
+/usr/bin/python /usr/local/src/myblog/online/index.py 9999 >/dev/null 2>&1  &
