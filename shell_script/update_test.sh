0c6a8e1a7bea627273ea3f3bdf102bd2a3fa1f91
wangqizhi <wqz@yiban.cn>
2015-01-14 16:17:15 +0800

    modify update_test.sh

--- shell_script/update_test.sh
+++ shell_script/update_test.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 /usr/bin/rsync --progress -av --exclude="blog_env.conf" --exclude="myUwsgi.ini" /root/src/git_hub/pyweber/* /usr/local/src/myblog/test
-# PID=`/bin/ps -ef |grep [p]ython |awk -F " " '{printf $2}'`
-# /bin/kill $PID
-# /usr/bin/python /usr/local/src/myblog/online/index.py 9999 >/dev/null 2>&1  &
+PID=`netstat -anlpt |grep 3032 |awk -F '/' '{print $1}'|awk -F ' ' '{print $7}'`
+/bin/kill -9 $PID
+/usr/local/python2.7.8/bin/uwsgi /usr/local/src/myblog/test/conf/myUwsgi.ini 2>&1 >/tmp/testWeb.log &
