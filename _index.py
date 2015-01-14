65444e715d534dea16c9c1b4f785f09f0a0d1726
wangqizhi <wqz@yiban.cn>
2015-01-14 18:07:41 +0800

    get path_info

--- _index.py
+++ _index.py
@@ -1,3 +1,3 @@
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html'),('Server','wqz_server')])
-    return ["Hello,World!"]
+    return ["path is "+env["PATH_INFO"]]
