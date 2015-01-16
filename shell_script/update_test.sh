4037b9dd6246c4fd842efc77777c951a3376323f
wangqizhi <wqz@yiban.cn>
2015-01-16 17:37:56 +0800

    add sh backup

--- shell_script/update_test.sh
+++ shell_script/update_test.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+tar zcvf /root/backup/test_bac.tar /usr/local/src/myblog/test
 /usr/bin/rsync --progress -av --exclude="blog_env.conf" --exclude="myUwsgi.ini" /root/src/git_hub/pyweber/* /usr/local/src/myblog/test
 PID=`netstat -anlpt |grep 3032 |awk -F '/' '{print $1}'|awk -F ' ' '{print $7}'`
 /bin/kill -9 $PID
