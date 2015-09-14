c13a9fb68c23be73a4c4d6e8821feafb67f157a0
coose.wang <wangizhi1987@gmail.com>
2015-09-14 22:07:02 +0800

    delete old

--- shell_script/update_online.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-/usr/bin/rsync --progress -av --exclude="blog_env.conf" /root/src/git_hub/pyweber/* /usr/local/src/myblog/online
-PID=`/bin/ps -ef |grep [p]ython |awk -F " " '{printf $2}'`
-/bin/kill $PID
-/usr/bin/python /usr/local/src/myblog/online/index.py 9999 >/dev/null 2>&1  &
--- shell_script/update_test.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-tar zcvf /root/backup/test_bac.tar /usr/local/src/myblog/test
-/usr/bin/rsync --progress -av --exclude="blog_env.conf" --exclude="myUwsgi.ini" /root/src/git_hub/pyweber/* /usr/local/src/myblog/test
-PID=`netstat -anlpt |grep 3032 |awk -F '/' '{print $1}'|awk -F ' ' '{print $7}'`
-/bin/kill -9 $PID
-/usr/local/python2.7.8/bin/uwsgi /usr/local/src/myblog/test/conf/myUwsgi.ini >/tmp/testWeb.log 2>&1  &
